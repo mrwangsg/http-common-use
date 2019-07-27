a2a5bb79bc9ce43cfebe7d60148309ce6c6dd735
shiguangwang <shiguang.wang@kingstarfintech.com>
2019-07-27 23:34:52 +0800

    实现文件上传 下载功能

---
+++ my-app/src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import { Input, Button} from 'antd';
+import $ from  'jquery';
+
+const style = {
+    margin : "25px"
+};
+
+
+class App extends Component {
+
+  handBtnClick = (event) => {
+    console.log($("#file")[0].files);
+    let fileList = $("#file")[0].files;
+
+    const form = new FormData();
+    form.append('file', fileList[0]);
+
+    $.ajax({
+        url: 'http://localhost:8080/upload/simple',
+        type: "POST",
+        data: form,
+        cache:false,
+        processData:false,
+        contentType:false,
+        success: function (data, textStatus) {
+            console.log("data: " + data)
+            console.log("textStatus: " + textStatus)
+            alert(data);
+        },
+        error: function (xhr, textStatus) {
+            console.log("xhr: " + xhr)
+            console.log("textStatus: " + textStatus)
+            alert(xhr);
+        }
+    });
+  };
+
+  render() {
+    return (
+      <div className="App" style={style}>
+          <Input id="file" name="file" type="file" style={{width:"20%"}}/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <Button type="primary" onClick={this.handBtnClick}>点击按钮</Button>
+          <div>
+              <a href=""></a>
+          </div>
+      </div>
+    );
+  };
+  
+}
+
+export default App;
